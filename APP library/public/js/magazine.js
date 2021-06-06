8758ee1542778f0542c5506be8ccc7c1343a3211
Ángel David Tapia Romo <77556008+Bearus-Dev@users.noreply.github.com>
2021-06-06 01:22:05 -0500

    Add files via upload

---
+++ APP library/public/js/magazine.js
@@ -0,0 +1,78 @@
+const db = firebase.firestore();
+//constructor de la clase
+class Art{
+    constructor(Autor, Year, Titulo, Place, Editorial, Url){
+        this.Autor = Autor;
+        this.Year = Year;
+        this.Titulo = Titulo;
+        this.Place= Place;
+        this.Editorial = Editorial;
+        this.Url = Url;
+    }
+}
+
+
+
+
+//Metodos de interfaz
+class UI {
+    addproduct(art){
+        const apaa = document.getElementById("product2");
+        const elementh = document.createElement("div");
+        elementh.innerHTML = `
+        <div class="card">
+                <div class="card-body">
+                    <strong> Bibliografia</strong>: ${art.Autor}. (${art.Year}). ${art.Titulo}. ${art.Place}, de  ${art.Editorial}. Recuperado de: ${art.Url}
+                </div>
+        </div>
+        `;
+        apaa.appendChild(elementh);
+        
+    }
+    resetForm(){
+        document.getElementById("product").reset();
+    }
+    showMessage(){
+        alert("faltan campos por rellenar");
+
+    }
+}
+const boton1 =document.getElementById("product");
+
+//Dom
+boton1.addEventListener("submit", async (e)=>{
+    e.preventDefault();
+//Obtiene el dato desde el html
+    const Autor = document.getElementById("Autor").value;
+    const Year = document.getElementById("Year").value;
+    const Titulo = document.getElementById("Titulo").value;
+    const Place = document.getElementById("Place").value;
+    const Editorial = document.getElementById("Editorial").value;
+    const Url = document.getElementById("Url").value;
+
+    if (Autor.value = null){
+     ui.showMessage();
+    }
+    else{
+        //muestra el dato en consola
+    const art = new Art(Autor, Year, Titulo, Place, Editorial, Url);
+    const ui = new UI();
+    ui.addproduct(art);
+    ui.resetForm();
+
+
+    
+    //mensaje de formulario enviado
+const response = await db.collection("apabook").doc().set({
+    Autor,
+    Year,
+    Titulo,
+    Place,
+    Editorial,
+    Url
+})
+    }
+
+
+
+});
---
+++ APP library/public/js/pop.js
@@ -0,0 +1,13 @@
+
+var firebaseConfig = {
+    apiKey: "AIzaSyD6_2fcS-cppZkNlLh7gDAzA3wzu-SoWFk",
+    authDomain: "apafree-839c5.firebaseapp.com",
+    databaseURL: "https://apafree-839c5-default-rtdb.firebaseio.com",
+    projectId: "apafree-839c5",
+    storageBucket: "apafree-839c5.appspot.com",
+    messagingSenderId: "905020787895",
+    appId: "1:905020787895:web:dad87f37253029cc65d132",
+    measurementId: "G-K3Y2X2KXZC"
+  };
+
+  firebase.initializeApp(firebaseConfig);
